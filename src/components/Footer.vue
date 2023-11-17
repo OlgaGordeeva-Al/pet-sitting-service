9eacd770e26ef2a677e00ec422d0a7e4e7de7f7e
Olga Gordeeva <olgagord93@gmail.com>
2023-11-17 14:38:11 +0300

    Added main files and routes structure

---
+++ src/components/Footer.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="footer">
+    <h1>footer</h1>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
