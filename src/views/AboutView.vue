2a6778c8658d3810e53b1ba3e1a7b0b85e6eb2da
Olga Gordeeva <olgagord93@gmail.com>
2023-11-22 15:24:21 +0300

    Done banner block

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -3,11 +3,38 @@
 </script>
 
 <template>
-  <div class="about">
-    <TextHeader header="Передержка питомцев" />
+  <div class="about-banner">
+    <div class="about-banner__text">
+      <TextHeader class="about-header" header="Передержка, выгул, ситтинг для вас и ваших питомцев" />
+      <p>Заботимся о ваших любимцах, как о своих 💜 </p>
+    </div>
+    <div class="about-banner__image">
+      <img src="undraw_passing_by_0un9.svg" alt="dog walking">
+    </div>
   </div>
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
+  .about-banner {
+    margin: 2rem 4rem;
 
+    &__text {
+      position: absolute;
+
+      p {
+        font-size: 20px;
+      }
+    }
+
+    .about-header {
+      width: 90%;
+    }
+
+    &__image {
+      position: relative;
+      display: flex;
+      justify-content: flex-end;
+      height: 70vh;
+    }
+  }
 </style>
