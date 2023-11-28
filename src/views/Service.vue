1932ea3ceeb2119a5a3f82ffb6bcfe6f93f79130
Olga Gordeeva <olgagord93@gmail.com>
2023-11-28 17:44:41 +0300

    Added some styles for service

--- src/views/Service.vue
+++ src/views/Service.vue
@@ -10,24 +10,46 @@ import {useRoute} from "vue-router";
     name: string;
     header: string,
     description: string,
-    servicesList: string[]
+    servicesList: string[],
+    imgSrc: string;
   }
 
   const data = servicesData.find(item => item.name === ser) as ServiceData;
 
-  const {header, description, servicesList} = data
+  const {header, description, servicesList, imgSrc} = data
 
 </script>
 
 <template>
-  <div>
-    <TextHeader class="service-header" :header="header" />
-    <p class="service-description">{{ description }}</p>
-    <ul class="service-list">
-      <p>Что входит в услугу</p>
-      <li v-for="item in servicesList" class="service-list__item">
-        {{ item }}
-      </li>
-    </ul>
+  <div class="service">
+    <div class="service-text">
+      <TextHeader class="service-text__header" :header="header" />
+      <p class="service-text__description">{{ description }}</p>
+      <ul class="service-list">
+        <p>Что входит в услугу</p>
+        <li v-for="item in servicesList" class="service-list__item">
+          {{ item }}
+        </li>
+      </ul>
+    </div>
+    <div class="service-image">
+      <img :src="imgSrc" :alt="header">
+    </div>
   </div>
 </template>
+
+<style scoped lang="scss">
+  .service {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    min-height: 100vh;
+    grid-gap: 20px;
+
+    .service-image {
+      img {
+        width: 100%;
+        height: 100%;
+      }
+    }
+  }
+</style>
